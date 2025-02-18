b126404317898fbe8890575948c6e2336f488c7d
isti96 <isti.daroczi@gmail.com>
2025-02-18 18:40:19 +0200

    setup cors

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const API_URL = "https://bankaccountdata.gocardless.com/api/v2";

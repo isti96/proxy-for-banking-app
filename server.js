bd110c397e980642cd6a99f5df0a1c4b4866ac1f
isti96 <isti.daroczi@gmail.com>
2025-02-20 14:04:59 +0200

    update server

--- server.js
+++ server.js
@@ -13,23 +13,51 @@ app.options("*", cors());
 
 app.use(express.json());
 
+const token = "";
 const API_URL = "https://bankaccountdata.gocardless.com/api/v2";
 
-app.use("/proxy", async (req, res) => {
+app.get("/gocardless/country", async (req, res) => {
+  console.log(req.body, "   ", req.query);
   try {
-    console.log(req.method);
-    console.log(`${API_URL}${req.originalUrl.replace("/proxy", "")}`);
-    console.log(req.body);
+    const { country } = req.query;
+
     const response = await axios({
       method: req.method,
-      url: `${API_URL}${req.originalUrl.replace("/proxy", "")}`,
+      url: `https://bankaccountdata.gocardless.com/api/v2/institutions/?country=${country}`,
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: req.body,
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/gocardless/token", async (req, res) => {
+  console.log(req.body, "   ", req.query);
+  try {
+    const response = await axios({
+      method: req.method,
+      url: "https://bankaccountdata.gocardless.com/api/v2/token/new",
       headers: {
         accept: "application/json",
         "Content-Type": "application/json",
       },
-      data: req.body,
+      data: {
+        secret_id: "66433513-7aed-4d19-bc80-7c9ac2ca0ba2",
+        secret_key:
+          "e270957e080f165c9f792106c134ad0706d7c696ded78087acc199ee06fdc364680778124d14efa9cf6521fcf215687d22126755fc160101bc3fe01862826ee9",
+      },
     });
-    res.json(response.data);
+    const data = await response.json();
+    console.log(data);
+    token = data.access;
+    res.json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
